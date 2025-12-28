8efe8a890b08eb3cf1ecaf865074226f1cd797c5
choeurnsophanny125-sketch <choeurnsophanny125@gmail.com>
2025-12-28 14:51:33 +0700

    fix main.yml 7

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import {createRouter, createWebHistory} from'vue-router';
+const routes =[
+  
+    {
+        path:'/',
+        name: 'home',
+        component: () => import('../components/Home.vue'),
+    },
+    {
+        path:'/about/us',
+        name: 'about',
+        component: () => import('@/components/About.vue'),
+    },
+    {
+        path:'/book',
+        name: 'book',
+        component: () => import('@/components/Book.vue'),
+    },
+    {
+        path:'/Search',
+        name: 'search',
+        component: () => import('@/components/Search.vue'),	
+    }
+
+];
+const router = createRouter({
+    history: createWebHistory(),routes
+});
+export default router;
