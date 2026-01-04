98d18cc78c5721d66ce992de5955b112ed4ed748
choeurnsophanny125-sketch <choeurnsophanny125@gmail.com>
2026-01-04 08:25:40 +0700

    Book Page lates

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -8,18 +8,225 @@
             <h3 class="text-gray-700 text-2xl font-medium">BOOK</h3>
             <span class="mt-3 text-sm text-gray-500">200+ Products</span>
             <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-                <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                    <div class="flex items-end justify-end h-56 w-full bg-cover" style="background-image: url('https://honwix.com/uploads/book/cover/412da79f2cab2eec29037c0b0be057268ba174fae5f039dee2a676c93c535f80.webp')">
-                        <button class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://honwix.com/uploads/book/cover/412da79f2cab2eec29037c0b0be057268ba174fae5f039dee2a676c93c535f80.webp"
+            alt="Twisted Love"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
                             <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                         </button>
-                    </div>
-                    <div class="px-5 py-3">
-                        <h3 class="text-gray-700 uppercase">Classic watch</h3>
-                        <span class="text-gray-500 mt-2">$123</span>
-                    </div>
-                </div>
-                <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">Twisted Love</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ_tRzTYsk7_BoiGc9Y7d3yJUsrgiqfJ7-3Q&s"
+            alt="Twisted Hate"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">Twisted Hate</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRNaiwv7bXXYLrm6bDm5oyFkicw17NMyfhcQ&s"
+            alt="Twisted Game"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">Twisted Game</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxDuoYKRT_RBqq-a_wcjkCZWwrEdKoqMIq8w&s"
+            alt="Twisted lie"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">Twisted Lies</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1659045816i/61767292._SX600_.jpg"
+            alt="Ice Breaker"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">Ice Breaker</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1671865608i/74045390._SX600_.jpg"
+            alt="The Fine Print"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">The Fine Print</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1684788871i/125157226._SX600_.jpg"
+            alt=" FlawLess"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase"> Flaw Less</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxPEnRwyC-2fy7txB5XWnfnUSOIG5bHtV4fA&s"
+            alt="The Untamed"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">The Untamed</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkngLJbAwjU9VyIy-DwAzP1XT29ZGieY5gyw&s"
+            alt="The Untamed2"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">The untamed 2</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT--dvnB29d4BismBbVBof7ML6AfVANhUmYUw&s"
+            alt="the untamed3"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">the untamed 3</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0beRbl75YXa_XwWEWbiiG-yXFFzEbgNnJsxjJ4CHgZQ&s"
+            alt="the untamed 4"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">the untamed 4</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+               <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+    <div class="relative w-full h-56 bg-gray-100 flex items-center justify-center">
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLcm50Y8bIS3av3bt_7hBLRx4pL_rSL9_IkpnkTDOsyg&s"
+            alt="the untamed 5"
+            class="max-h-full max-w-full object-contain"
+        />
+
+        <button class="p-2 rounded-full bg-blue-600 text-white  -mb-55 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                            <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                        </button>
+    </div>
+
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">the untamed 5</h3>
+        <span class="text-gray-500 mt-2">$12</span>
+    </div>
+</div>
+
+
+                <!-- <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
                     <div class="flex items-end justify-end h-56 w-full bg-cover" style="background-image: url('https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80')">
                         <button class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
                             <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
@@ -183,7 +390,7 @@
                         <h3 class="text-gray-700 uppercase">breitling watch</h3>
                         <span class="text-gray-500 mt-2">$180</span>
                     </div>
-                </div>
+                </div> -->
             </div>
             <div class="flex justify-center">
                 <div class="flex rounded-md mt-8">
--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router';
-import "./assets/css/bootstrap.css";
-import "./assets/css/responsive.css";
-//import "./assets/image" ;
-// import "./assets/css/font-awesome.min.css";
+
 createApp(App).use(router).mount('#app')
 
